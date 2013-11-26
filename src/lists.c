1422efcfa1ab00fec19f31e25766bbedbee8b9b1
Avi <Avi@admintheweb.com>
2013-11-27 01:12:38 +0200

    Update file name and gitignore

--- src/lists.c
+++ src/lists.c
@@ -1,10 +1,10 @@
 /*
  ============================================================================
- Name        : Maman-21.c
+ Name        : lists.c
  Author      : Avi Levy
  Version     :
  Copyright   : All Right for Avi Levy
- Description : Hello World in C, Ansi-style
+ Description : Maman 21 in C, Ansi-style
  ============================================================================
  */
 
@@ -12,6 +12,12 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+	char choise;
+
+	printf("/nplese choise how to store the input:/n");
+	printf("1. conected list/n");
+	printf("2. array in memory/n");
+	printf("enter your choise:");
+	choise = getchar();
+	return 0;
 }
