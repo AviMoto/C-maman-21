5e60b371eb7aba55f1c9fa312d9f22e179de65d8
Avi <Avi@admintheweb.com>
2013-11-27 00:50:08 +0200

    Update files

---
+++ src/Maman-21.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Maman-21.c
+ Author      : Avi Levy
+ Version     :
+ Copyright   : All Right for Avi Levy
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
