f5a48ddfaa15b17fa869c306656e6f92757463cf
Avi <Avi@admintheweb.com>
2013-11-28 21:11:47 +0200

    update

--- src/lists.c
+++ src/lists.c
@@ -13,24 +13,45 @@
 #include "lists.h"
 
 int main(void) {
-	char choise=0, *string;
+	int index=0, interval=1;
+	char *string, choise=0;
 
 	printf("\nplease chose how to store the input:\n");
 	printf("1. connected list\n");
 	printf("2. array in memory\n");
 	printf("enter your chose:");
 	choise = getchar();
+	printf("\nStart to enter your string\n");
 	switch(choise){
 		case '1':
 			break;
 		case '2':
-			string = malloc(10*sizeof(char));
-			if (string != 0){
-
+			while((choise = getchar())!= '\n' || choise == EOF){
+				if(index == STRING_BLOCK){
+					string = (char *)realloc(string, STRING_BLOCK*interval*sizeof(char));
+				} else {
+					string = (char *)malloc(STRING_BLOCK*sizeof(char));
+				}
+				if (string != 0){
+					for(index=STRING_BLOCK*(interval-1);index<STRING_BLOCK;++index){
+						*(string+index) = choise;
+						if((choise = getchar())== '\n' || choise == EOF){
+							break;
+						}
+					}
+					if(index != STRING_BLOCK){
+						break;
+					}
+					++interval;
+				}
 			}
 			break;
 		default:
 			break;
+	}
+	printf("the string we get is %s",string);
+	if(string != 0){
+		free(string);
 	}
 	return 0;
 }
